4aae1061c1eddcdbd5d24b6a8e8036294ab242f5
aVozgrin <anton.vozgrin@gmail.com>
2016-07-18 00:39:42 +0300

    ch

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+ $( ".logo" ).click(function( event ) {
+ 				$(this).animate({
+ 				top:"+=200"
+ 				},5000);
+ 				/*$(this).addClass("test");*/
+        /*alert( "Thanks for visiting!" );*/
+ 
+    });
+});
